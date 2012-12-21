b19c0febdaacd795f91675c1a17aa832a8d8ceca
Jared Tobin <jared@jtobin.ca>
2012-12-22 00:12:59 +1300

    Fix unit test path.

--- tests/test.sh
+++ tests/test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-../dist/build/prompt-pcap/prompt-pcap -r ../data/mdf-kospi200.20110216-0.pcap | -cut -f 4-5 -d ' ' | sort -c
+./dist/build/prompt-pcap/prompt-pcap -r ./data/mdf-kospi200.20110216-0.pcap | cut -f 4-5 -d ' ' | sort -c
