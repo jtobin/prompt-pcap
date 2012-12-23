30556c25543f09145a0627a76da2b8c35f77ab4f
Jared Tobin <jared@jtobin.ca>
2012-12-23 19:31:53 +1300

    Final polish.

--- tests/test.sh
+++ tests/test.sh
@@ -9,6 +9,6 @@ reorderLines=$(./dist/build/prompt-pcap/prompt-pcap ./data/mdf-kospi200.20110216
 $(./dist/build/prompt-pcap/prompt-pcap ./data/mdf-kospi200.20110216-0.pcap -r | cut -f 4-5 -d ' ' | sort -c)
 ordered=$?
 
-# summary
+# test summary
 [[ ($vanillaLines = $reorderLines) && $ordered ]] && echo "All tests passed."
 
